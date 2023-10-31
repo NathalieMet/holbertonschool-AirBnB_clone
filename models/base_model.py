1be70937e1f45f6f8c5bee919db2de90697cdd0b
nathalie <nathalie.metreau@hotmail.fr>
2023-10-31 14:10:34 +0100

    add some comments

--- models/base_model.py
+++ models/base_model.py
@@ -1,9 +1,7 @@
 #!/bin/python3
+"""Python console"""
 import uuid
 from datetime import datetime
-
-
-"""Python console"""
 
 
 class BaseModel:
@@ -11,19 +9,23 @@ class BaseModel:
     """class Basemodel"""
 
     def __init__(self):
+        """initialize instance"""
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
     def __str__(self):
+        """print instances"""
         return ("[{}] ({}) {}"
                 .format(self.__class__.__name__, self.id, self.__dict__))
 
     def save(self):
+        """update the time of the last instance"""
         self.updated_at = datetime.now()
         self.updated_at.isoformat()
 
     def to_dict(self):
+        """do a copy of dictionnary"""
         obj_dict = self.__dict__.copy()
         obj_dict['__class__'] = self.__class__.__name__
         obj_dict['update_at'] = self.updated_at.isoformat()
