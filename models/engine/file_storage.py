0a44dfebe12b1505de3fdef73703f4aef137dc53
ER1-53 <cremey.erwan@gmail.com>
2023-11-01 12:05:53 +0100

    update pycodestyle

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,11 +1,10 @@
 #!/bin/python3
 """save in storage"""
 import json
-import os
 
 
 class FileStorage(BaseException):
-        
+
     __file_path = 'file.json'
     __objects = {}
 
@@ -15,15 +14,15 @@ class FileStorage(BaseException):
     def new(self, obj):
         key = "{}.{}".format(obj.__class__.__name__, obj.id)
         self.__objects[key] = obj.to_dict()
-        
+
     def save(self):
-        with open (self.__file_path, 'w', encoding='UTF-8') as file:
+        with open(self.__file_path, 'w', encoding='UTF-8') as file:
             json.dump(self.__objects, file)
 
     def reload(self):
         try:
             if self.__file_path:
-                with open (self.__file_path, 'r', encoding='UTF-8') as file:
-                    self.__objects =  json.load(file)
+                with open(self.__file_path, 'r', encoding='UTF-8') as file:
+                    self.__objects = json.load(file)
         except Exception:
-                    pass
+            pass
