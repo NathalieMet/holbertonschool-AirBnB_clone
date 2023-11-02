0a94f4dce5a71b26402f85e297683c964a5d0b3e
ER1-53 <cremey.erwan@gmail.com>
2023-11-02 18:08:20 +0100

    update task 5

--- models/base_model.py
+++ models/base_model.py
@@ -29,7 +29,8 @@ class BaseModel:
         else:
             self.updated_at = self.created_at = datetime.now()
             self.id = str(uuid.uuid4())
-            storage.new(self)
+
+        storage.new(self)
 
     def __str__(self):
         """Returns a string representation of the instance.
@@ -46,6 +47,7 @@ class BaseModel:
 
         self.updated_at = datetime.now()
         storage.save()
+        storage.new(self)
 
     def to_dict(self):
         """Returns a copy of the instance data in dictionary format.
